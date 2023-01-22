ba5efeb1a8ca93d9c116e728fef4d09ac4ddd1ca
Abdoelrahman Eaita <abdoelrahman.eaita@adevinta.com>
2023-01-22 15:30:14 +0100

    Initial Commit

---
+++ TCAUsecase/TCAUsecaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCAUsecaseApp.swift
+//  TCAUsecase
+//
+//  Created by abdoelrahman.eaita on 22/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct TCAUsecaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
